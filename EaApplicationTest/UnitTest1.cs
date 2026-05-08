f5b9708fa1888a2319009edfcfb08c3fa847ad26
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-05-08 10:39:17 -0400

    Add .gitignore, improve solution structure, and enhance UI tests
    - Added a comprehensive `.gitignore` file.
    - Introduced "Solution Items" folder to include files like `.gitignore` in the solution.
    - Enhanced `UnitTest1` by refactoring tests to utilize `IDisposable` for cleaner resource management.
    - Added a second test case to cover additional scenarios.

--- EaApplicationTest/UnitTest1.cs
+++ EaApplicationTest/UnitTest1.cs
@@ -4,25 +4,55 @@ using OpenQA.Selenium.Support.UI;
 
 namespace EaApplicationTest;
 
-public class UnitTest1
+public class UnitTest1 : IDisposable
 {
+    private readonly ChromeDriver _driver;
+
+    public UnitTest1()
+    {
+        _driver = new ChromeDriver();
+    }
+    
     [Fact]
     public void Test1()
     {
-        var driver = new ChromeDriver();
-        driver.Navigate().GoToUrl("http://localhost:8000");
+        _driver.Navigate().GoToUrl("http://localhost:8000");
 
-        driver.FindElement(By.LinkText("Product")).Click();
+        _driver.FindElement(By.LinkText("Product")).Click();
         
-        driver.FindElement(By.LinkText("Create")).Click();
+        _driver.FindElement(By.LinkText("Create")).Click();
 
-        driver.FindElement(By.Name("Name")).SendKeys("Product 1");
-        driver.FindElement(By.Name("Description")).SendKeys("Description 1");
-        driver.FindElement(By.Name("Price")).SendKeys("100");
+        _driver.FindElement(By.Name("Name")).SendKeys("Product 1");
+        _driver.FindElement(By.Name("Description")).SendKeys("Description 1");
+        _driver.FindElement(By.Name("Price")).SendKeys("1000");
 
-        var select = new SelectElement(driver.FindElement(By.Name("ProductType")));
+        var select = new SelectElement(_driver.FindElement(By.Name("ProductType")));
         select.SelectByText("CPU");
         
-        driver.FindElement(By.Id("Create")).Submit();
+        _driver.FindElement(By.Id("Create")).Submit();
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        _driver.Navigate().GoToUrl("http://localhost:8000");
+
+        _driver.FindElement(By.LinkText("Product")).Click();
+        
+        _driver.FindElement(By.LinkText("Create")).Click();
+
+        _driver.FindElement(By.Name("Name")).SendKeys("Product 2");
+        _driver.FindElement(By.Name("Description")).SendKeys("Description 2");
+        _driver.FindElement(By.Name("Price")).SendKeys("2000");
+
+        var select = new SelectElement(_driver.FindElement(By.Name("ProductType")));
+        select.SelectByText("CPU");
+        
+        _driver.FindElement(By.Id("Create")).Submit();
+    }
+
+    public void Dispose()
+    {
+        _driver.Dispose();
     }
 }
